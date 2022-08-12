f7c236483995eb4bbda8074c11fed6f6f7c93c4f
Benjamin Such <benjamin.such@gmail.com>
2022-08-12 17:44:32 +0200

    initial commit

---
+++ packages/create-apex/README.md
@@ -0,0 +1,29 @@
+# create-apex
+
+A CLI to scaffold a game for the Apex Engine.
+
+## Get Started
+
+```bash
+# Yarn
+$ yarn create apex
+
+# or with NPM
+$ npm create apex@latest
+```
+
+Continue by following the prompt messages popping up.
+
+You can also directly specify the project destination as arguments:
+
+```bash
+# Yarn
+$ yarn create apex ~/myprojects/MyCoolGame
+
+# npm
+$ npm create apex@latest ~/myprojects/MyCoolGame
+```
+
+Additionally to the project destination, you can specify various options (see below).
+
+## Options
---
+++ packages/create-apex/index.js
@@ -0,0 +1,145 @@
+#!/usr/bin/env node
+import { Command } from 'commander';
+import { copyFileSync, existsSync, mkdirSync, readdirSync, readFileSync, renameSync, rmSync, statSync, writeFileSync } from 'node:fs';
+import { homedir } from 'node:os';
+import { basename, join, relative, resolve } from 'node:path';
+import chalk from 'chalk';
+import prompts from 'prompts';
+const createApexPkg = JSON.parse(readFileSync('package.json', 'utf-8'));
+const defaultProjectName = 'my-project';
+const program = new Command();
+program
+    .name(createApexPkg.name)
+    .description(createApexPkg.description)
+    .version(createApexPkg.version);
+program
+    .argument('[name]', 'Name of your project.')
+    .action(async (initialDir = '', { skipOptional }) => {
+    let targetDir = resolve(initialDir === '.' ? '' : initialDir);
+    if (initialDir[0] === '~') {
+        targetDir = join(homedir(), initialDir.substring(1));
+    }
+    const projectName = initialDir === '.' ? basename(resolve()) : initialDir;
+    const cwd = process.cwd();
+    try {
+        const { packageName = projectName, overwrite } = await prompts([
+            {
+                type: initialDir ? null : 'text',
+                name: 'projectName',
+                message: 'Project name:',
+                initial: defaultProjectName,
+                onState({ value = defaultProjectName }) {
+                    targetDir = resolve(sanitizePath(value));
+                }
+            },
+            {
+                type: isValidDir(targetDir) ? null : 'confirm',
+                name: 'overwrite',
+                message: `Target directory "${targetDir}" exists already. Do you want to continue (existing files will be removed)?`
+            },
+            {
+                type: isValidPackageName(projectName) ? null : 'text',
+                name: 'packageName',
+                message: 'Package name:',
+                initial: toValidPackageName(projectName),
+                validate(val) {
+                    return isValidPackageName(val) || 'Invalid package name';
+                }
+            },
+            {
+                type: skipOptional ? null : 'confirm',
+                name: 'installBlender',
+                message: 'Do you want to install Blender with the ApexEngine add-on?',
+                initial: false
+            }
+        ], {
+            onCancel() {
+                throw new Error(chalk.red('✖') + ' Operation cancelled');
+            },
+            onSubmit(_, __, { overwrite } = {}) {
+                if (overwrite === false) {
+                    throw new Error(chalk.red('✖') + ' Operation cancelled');
+                }
+            }
+        });
+        if (overwrite) {
+            rmSync(targetDir, { recursive: true, force: true });
+        }
+        console.log(`\nCreating project files in ${targetDir}...`);
+        copyProjectFiles(targetDir, projectName);
+        updatePkg(targetDir, { name: packageName });
+        console.log(`\n${chalk.green('√')} Done. Continue by running:\n`);
+        if (targetDir !== cwd) {
+            console.log(`  cd ${relative(cwd, targetDir)}`);
+        }
+        const { commands } = getPkgManager();
+        console.log(`  ${commands.install}`);
+        console.log(`  ${commands.dev}`);
+        console.log();
+    }
+    catch (error) {
+        if (error instanceof Error) {
+            console.log(error.message);
+        }
+    }
+});
+program.parse();
+function getPkgManager() {
+    const { npm_config_user_agent = '' } = process.env;
+    const [manager] = npm_config_user_agent.split(' ');
+    const [name, version] = manager.split('/');
+    const commands = {
+        yarn: {
+            install: 'yarn',
+            dev: 'yarn dev'
+        },
+        npm: {
+            install: 'npm install',
+            dev: 'npm run dev'
+        }
+    };
+    return { name, version, commands: commands[name] };
+}
+function sanitizePath(path = '') {
+    return path?.trim().replace(/\/+$/g, '');
+}
+function isValidDir(dir) {
+    return !existsSync(dir) || readdirSync(dir).length < 1;
+}
+function toValidPackageName(name) {
+    return name
+        .trim()
+        .toLowerCase()
+        .replace(/\s+/g, '-')
+        .replace(/^[._]/, '')
+        .replace(/[^a-z0-9-~]+/g, '-');
+}
+function isValidPackageName(name) {
+    return /^(?:@[a-z0-9-*~][a-z0-9-*._~]*\/)?[a-z0-9-~][a-z0-9-._~]*$/.test(name);
+}
+function updatePkg(dir, content) {
+    const pkg = JSON.parse(readFileSync(join(dir, `package.json`), 'utf-8'));
+    writeFileSync(join(dir, `package.json`), JSON.stringify({ ...pkg, ...content }, null, 2));
+}
+function copyProjectFiles(dest, projectName = defaultProjectName) {
+    if (!existsSync(dest)) {
+        mkdirSync(dest);
+    }
+    copyDir('template', dest);
+    renameSync(`${dest}/${defaultProjectName}.blend`, `${dest}/${projectName}.blend`);
+}
+function copy(src, dest) {
+    const stat = statSync(src);
+    if (stat.isDirectory()) {
+        copyDir(src, dest);
+    }
+    else {
+        copyFileSync(src, dest);
+    }
+}
+function copyDir(src, dest) {
+    mkdirSync(dest, { recursive: true });
+    for (const file of readdirSync(src)) {
+        copy(resolve(src, file), resolve(dest, file));
+    }
+}
