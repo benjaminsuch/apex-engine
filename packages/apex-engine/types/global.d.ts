3a86c9cca53441dd7f394661fd7a2fcc5d2c8288
Benjamin S <706852+benjaminsuch@users.noreply.github.com>
2022-08-17 16:30:44 +0200

    add boilerplate game content

--- packages/apex-engine/types/global.d.ts
+++ packages/apex-engine/types/global.d.ts
@@ -1,6 +1,7 @@
 declare global {
   const IS_CLIENT: boolean;
+  const IS_DEDICATED_SERVER: boolean;
+  const IS_LISTEN_SERVER: boolean;
   const IS_SERVER: boolean;
+  const IS_STANDALONE: boolean;
 }
-
-export {};
--- packages/create-apex-game/template/vite.config.ts
+++ packages/create-apex-game/template/vite.config.ts
@@ -1,10 +1,26 @@
-import { defineConfig } from 'vite';
+import replace from '@rollup/plugin-replace';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     outDir: 'build/browser'
   },
-  plugins: [react()]
+  server: {
+    port: 3000
+  },
+  plugins: [
+    react(),
+    replace({
+      preventAssignment: true,
+      values: {
+        IS_CLIENT: 'false',
+        IS_DEDICATED_SERVER: 'false',
+        IS_LISTEN_SERVER: 'false',
+        IS_SERVER: 'false',
+        IS_STANDALONE: 'false'
+      }
+    })
+  ]
 });
