8aaca98fc722f61f97da6a89240c7f456d662ad1
Benjamin Such <benjamin.such@gmail.com>
2023-07-12 11:04:08 +0200

    assign port and host from env vars

--- packages/apex-engine/src/launch/node/index.ts
+++ packages/apex-engine/src/launch/node/index.ts
@@ -11,16 +11,16 @@ import { CliMain } from './CliMain';
  * @param args CLI arguments for the engine (not to be confused with arguments of our build-scripts).
  */
 export async function main(args: string[]) {
-  const hostname = '127.0.0.1';
-  const port = 3010;
+  const { HOST = '127.0.0.1', PORT = 3010 } = process.env;
+
   const server = createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     res.end('Hello World');
   });
 
-  server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+  server.listen(PORT, HOST as any, () => {
+    console.log(`Server running at http://${HOST}:${PORT}/`);
   });
 
   const cli = new CliMain(args);
