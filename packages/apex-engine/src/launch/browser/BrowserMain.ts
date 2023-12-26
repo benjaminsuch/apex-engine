19d9c69028b89162d81df154d3ec216461efb456
Benjamin Such <benjamin.such@gmail.com>
2023-12-26 21:13:37 +0100

    remove engine loop tick call from main thread

--- packages/apex-engine/src/launch/browser/BrowserMain.ts
+++ packages/apex-engine/src/launch/browser/BrowserMain.ts
@@ -13,9 +13,6 @@ export class BrowserMain {
 
   public init(): void {
     const engineLoop = this.instantiationService.createInstance(EngineLoop);
-
-    engineLoop.init().then(() => {
-      engineLoop.tick();
-    });
+    engineLoop.init();
   }
 }
