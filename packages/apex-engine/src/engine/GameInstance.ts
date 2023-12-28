95feeb7cf2b231537de0d1f323e81a5287e285c6
Benjamin Such <benjamin.such@gmail.com>
2023-12-28 17:27:53 +0100

    add boilerplate code for World to call beginPlay

--- packages/apex-engine/src/engine/GameInstance.ts
+++ packages/apex-engine/src/engine/GameInstance.ts
@@ -1,15 +1,34 @@
+import { IInstantiationService } from '../platform/di/common/InstantiationService';
+import { IConsoleLogger } from '../platform/logging/common/ConsoleLogger';
 import { type ApexEngine } from './ApexEngine';
+import { World } from './World';
 
 export class GameInstance {
-  constructor(private readonly engine: ApexEngine) {}
+  private world: World | null = null;
+
+  public getWorld(): World {
+    if (!this.world) {
+      throw new Error(`No world set.`);
+    }
+    return this.world;
+  }
+
+  constructor(
+    private readonly engine: ApexEngine,
+    @IInstantiationService protected readonly instantiationService: IInstantiationService,
+    @IConsoleLogger protected readonly logger: IConsoleLogger
+  ) {}
 
   public init(): void {
+    this.logger.debug(this.constructor.name, 'Initialize');
 
+    this.world = this.instantiationService.createInstance(World);
+    this.world.init(this);
   }
 
   public start(): void {
     this.engine.loadMap(DEFAULT_MAP).then(() => {
-
+      this.getWorld().beginPlay();
     });
   }
 }
--- packages/apex-engine/src/engine/World.ts
+++ packages/apex-engine/src/engine/World.ts
@@ -1,3 +1,28 @@
-export class World {
+import { IConsoleLogger } from '../platform/logging/common/ConsoleLogger';
+import { type GameInstance } from './GameInstance';
 
+export class World {
+  private gameInstance: GameInstance | null = null;
+
+  public getGameInstance(): GameInstance {
+    if (!this.gameInstance) {
+      throw new Error(`No game instance set.`);
+    }
+    return this.gameInstance;
+  }
+
+  public isInitialized: boolean = false;
+
+  constructor(@IConsoleLogger protected readonly logger: IConsoleLogger) {}
+
+  public init(gameInstance: GameInstance): void {
+    this.logger.debug(this.constructor.name, 'Initialize');
+
+    this.gameInstance = gameInstance;
+    this.isInitialized = true;
+  }
+
+  public beginPlay(): void {
+    this.logger.debug(this.constructor.name, 'Begin Play');
+  }
 }
