781388f3dc1190c2a91686627f7257cc9bda217f
Benjamin Such <benjamin.such@gmail.com>
2024-01-22 09:53:21 +0100

    fix typo

--- packages/apex-engine/src/engine/renderer/RenderWorkerContext.ts
+++ packages/apex-engine/src/engine/renderer/RenderWorkerContext.ts
@@ -23,7 +23,7 @@ export class RenderWorkerContext implements IRenderWorkerContext {
   public getRenderingInfo(): RenderingInfo {
     if (!this.rendererInfo) {
       throw new Error(
-        `The renderer1 info is not available yet. The renderer has most likely not finished his initialization or is not running.1`
+        `The renderer info is not available yet. The renderer has most likely not finished his initialization or is not running.`
       );
     }
     return this.rendererInfo;
