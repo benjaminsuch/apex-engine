c50d8af84750e663475593122e1fbe442e5d7ea5
Benjamin Such <benjamin.such@gmail.com>
2024-01-25 00:57:23 +0100

    fix formatting

--- packages/apex-engine/src/engine/core/array.ts
+++ packages/apex-engine/src/engine/core/array.ts
@@ -1,11 +1,11 @@
 declare interface Array<T> {
   /**
- * Removes the item at `idx`.
- *
- * This method is way more performant than using `splice`. It swaps the item at `idx`
- * with the last item and then calls `pop` to remove it. Doing so will prevent the
- * compiler to re-arrange the array.
- */
+   * Removes the item at `idx`.
+   *
+   * This method is way more performant than using `splice`. It swaps the item at `idx`
+   * with the last item and then calls `pop` to remove it. Doing so will prevent the
+   * compiler to re-arrange the array.
+   */
   removeAtSwap(idx: number): void;
 }
 
