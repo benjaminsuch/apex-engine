f1663547873f9b06d114ab17930a744cd9e4f884
Benjamin Such <benjamin.such@gmail.com>
2024-04-19 13:35:56 +0200

    add http service to electron main-launcher

--- packages/apex-engine/src/launch/electron-sandbox/WindowMain.ts
+++ packages/apex-engine/src/launch/electron-sandbox/WindowMain.ts
@@ -3,6 +3,8 @@ import PhysicsWorker from '../../engine/worker/browser/physics?worker';
 import RenderWorker from '../../engine/worker/browser/render?worker';
 import { InstantiationService } from '../../platform/di/common/InstantiationService';
 import { ServiceCollection } from '../../platform/di/common/ServiceCollection';
+import { BrowserHttpService } from '../../platform/http/browser/HttpService';
+import { IHttpService } from '../../platform/http/common/Http';
 import { ConsoleLogger, IConsoleLogger } from '../../platform/logging/common/ConsoleLogger';
 import { BrowserWorkerManager } from '../../platform/worker/browser/BrowserWorkerManager';
 import { IWorkerManager } from '../../platform/worker/common/WorkerManager';
@@ -12,7 +14,10 @@ export class WindowMain {
 
   constructor() {
     const services = new ServiceCollection();
-    services.set(IConsoleLogger, new ConsoleLogger());
+    const logger = new ConsoleLogger();
+
+    services.set(IConsoleLogger, logger);
+    services.set(IHttpService, new BrowserHttpService(logger));
 
     this.instantiationService = new InstantiationService(services);
 
